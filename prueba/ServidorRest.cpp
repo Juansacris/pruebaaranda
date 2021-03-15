e9c51aac376365e326208a44df622bceb43cc209
Juansacris <sacristanvargas@hotmail.com>
2021-03-14 20:41:31 -0500

    Impementacion de validaciones

--- prueba/ServidorRest.cpp
+++ prueba/ServidorRest.cpp
@@ -13,34 +13,65 @@ void ServidorRest::handleGet(http_request mensaje)
 	}
 	if (rutas[0] == U("arboles"))
 	{
+		if (rutas.size() == 3) {
+			utility::stringstream_t ss(rutas[1]);
+			unsigned long long id;
+			ss >> id;
+			Nodo* arbol;
+			try {
+				arbol = _bd.consultar(id);
+			}
+			catch(std::exception&exc){
+				std::string error(exc.what());
+				mensaje.reply(status_codes::BadRequest, utility::conversions::to_string_t(error));
+				return;
 
-		utility::stringstream_t ss(rutas[1]);
-		unsigned long long id;
-		ss >> id;
+			}
+			if (arbol == nullptr) {
+				mensaje.reply(status_codes::NotFound, U("El arbol no existe"));
+				return;
+			}
 
-		Nodo* arbol = _bd.consultar(id);
 
-
-		if (rutas[2] == U("ancestro"))
-		{
-			Ancestro ancestro(arbol);
-			auto valores = mensaje.extract_json().get();
-			int nodo1 = valores[U("nodo1")].as_integer();
-			int nodo2 = valores[U("nodo2")].as_integer();
-
-			std::optional<int> ancestroComun = ancestro.encontrarAncestro(nodo1, nodo2);
-			if (ancestroComun)
+			if (rutas[2] == U("ancestro"))
 			{
-				json::value respuesta;
-				respuesta[U("Ancestro")] = ancestroComun.value();
-				mensaje.reply(status_codes::OK, respuesta);
+				Ancestro ancestro(arbol);
+				auto valores = mensaje.extract_json().get();
+				if (valores.has_integer_field(U("nodo1")) && valores.has_integer_field(U("nodo2")))
+				{
+
+
+					int nodo1 = valores[U("nodo1")].as_integer();
+					int nodo2 = valores[U("nodo2")].as_integer();
+
+					std::optional<int> ancestroComun = ancestro.encontrarAncestro(nodo1, nodo2);
+					if (ancestroComun)
+					{
+						json::value respuesta;
+						respuesta[U("Ancestro")] = ancestroComun.value();
+						mensaje.reply(status_codes::OK, respuesta);
+						return;
+					}
+					else
+					{
+						mensaje.reply(status_codes::NotFound, U("Alguno de los nodos no pertence al arbol"));
+					}
+
+				}
+				else
+				{
+					mensaje.reply(status_codes::NotFound, U("Debe colocar los nodos a consultar"));
+				}
+			}
+			else
+			{
+				mensaje.reply(status_codes::NotFound, U("No se encuentra el nodo"));
 			}
 		}
 		else
 		{
-			mensaje.reply(status_codes::NotFound, U("No se encuentra el nodo"));
+			mensaje.reply(status_codes::NotFound, U("Url Invalida"));
 		}
-
 	}
 
 	mensaje.reply(status_codes::NotFound, U("No se encuentra la url"));
